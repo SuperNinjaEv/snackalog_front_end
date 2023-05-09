102a3787078e6c0bf4efaac62b536e04935554d7
Felizj17 <felizj17@gmail.com>
2023-05-09 16:06:18 -0400

    navbar styling added and "custom logo"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Snacks from './components/Snacks';
-//import useState here since it will affect the list or grid view IN THE HOMEPAGE 
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Navbar from './components/Navbar'
+import Snacks from './components/Snacks'
+//import useState here since it will affect the list or grid view IN THE HOMEPAGE
 
 function App() {
   return (
-    <div className="App">
-      <Snacks />
+    <div className='App'>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/snacks' element={<Snacks />} />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
