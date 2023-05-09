3e75fd96a4318c833d3ff3821cc242288a35e8bc
MichelBDLC <michelbdlc@gmail.com>
2023-05-08 22:23:19 -0700

    api data gather and display

--- src/components/Snack.js
+++ src/components/Snack.js
@@ -1,12 +1,35 @@
-import { NavLink } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function Snack() {
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
+
+export default function Snack(props) {
+
+    let navigate = useNavigate();
+    const [snack, setSnack] = useState({});
+    let { id } = useParams();
+
+    useEffect(() => {
+        axios.get(`${REACT_APP_API_URL}/snacks/${id}`)
+        .then((response) => {
+            setSnack(response.data)
+        }).catch((e) => {
+            console.error("catch", e)
+            navigate("*" || "/not-found")
+            //update to working route after testing
+        })
+    }, [id, navigate]);
 
     return (
         <>
-        <div className="snack-card">
-            <p> Snack info will be here </p>
+        <div className="snack-card" key={props.id}>
+            {/* make img and name links for clicanle to more details page  */}
+            <img src={props.url} />
+            <p> {props.name} </p>
         </div>
         </>
     )
 }
+
+//include button to go back to homepage of lists of snacks
--- src/components/Snacks.js
+++ src/components/Snacks.js
@@ -1,11 +1,45 @@
+import axios from "axios";
 import Snack from "./Snack";
+import { useState, useEffect } from "react";
+
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
 export default function Snacks() {
 
-    const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+    const [snacks, setSnacks] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${REACT_APP_API_URL}/snacks`)
+        .then((response) => setSnacks(response.data))
+        .catch((e) => console.error("catch", e))
+    }, []);
 
     return (
         <>
+        {
+            snacks.map((indSnack) => {
+        
+                let name = indSnack.name;
+                let category = indSnack.category;
+                let fiber = indSnack.fiber;
+                let id = indSnack.id;
+                let favorite = indSnack.is_favorite;
+                let gluten = indSnack.is_glutenfree;
+                let vegan = indSnack.is_vegan;
+                let vegetarian = indSnack.is_vegetarian;
+                let protein = indSnack.protein;
+                let sugar = indSnack.sugar;
+                let url = indSnack.url
+        
+                return (
+                    <>
+                    <br />
+                    <Snack name={name} category={category} fiber={fiber} id={id} favorite={favorite} gluten={gluten} vegan={vegan}
+                    vegetarian={vegetarian} protein={protein} sugar={sugar} url={url} />
+                    </>
+                )
+            })
+        }
         </>
     )
 }
