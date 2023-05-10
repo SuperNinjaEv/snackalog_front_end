356dba07d96c6a648f18e87febf8a27a785ccb01
MichelBDLC <michelbdlc@gmail.com>
2023-05-09 20:46:52 -0700

    commit before merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Snacks from './components/Snacks';
+import Snacks from './Components/Snacks';
 //import useState here since it will affect the list or grid view IN THE HOMEPAGE...perhaps 
 //might need to add lazyloading for faster website data display
 
